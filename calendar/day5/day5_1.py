b45fdf7b8dbf1f97376d7325d1d03bf68507052a
Matas <98917854+shrub2@users.noreply.github.com>
2024-12-14 10:23:26 +0200

    Day 5 Part 1

--- calendar/day5/day5_1.py
+++ calendar/day5/day5_1.py
@@ -8,12 +8,28 @@ page_numbers = input_text[1].strip().split("\n")
 
 page_rules_dict = {}
 for page_rule in page_rules:
-    key, value = map(int, page_rule.split("|"))
+    key, value = page_rule.split("|")
     page_rules_dict.setdefault(key, []).append(value)
 
 page_numbers_list = []
 for line in page_numbers:
-    page_numbers_list.append(list(map(int, line.split(","))))
+    page_numbers_list.append(line.split(","))
 
 
+result = 0
+for numbers in page_numbers_list:
+    valid_list = True
+    for index, number in enumerate(numbers):
+        try:
+            rules = page_rules_dict[number]
+        except KeyError:
+            rules = []
+            continue
+        numbers_before = numbers[:index]
+        if set(rules) & set(numbers_before):
+            valid_list = False
+            break
+    if valid_list:
+        result += int(numbers[len(numbers) // 2])
 
+print(result)
